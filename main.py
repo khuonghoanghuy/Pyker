9eb2dd3f413e9a90db6f24cfc7ed0aa69c488b9e
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-01-26 18:43:21 +0700

    added images

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 import saveData
 
 saveData.loadData()
@@ -10,7 +9,12 @@ pygame.display.set_caption("PyKer Game")
 
 text = pygame.font.Font(None, 54).render('Score: ' + str(saveData.loadData()['score']), True, (255, 255, 255))
 text_rect = text.get_rect(center=(35, 35))
-pygame.display.get_surface().blit(text, (0, 0))
+pygame.display.get_surface().blit(text, (35, 35))
+pygame.display.flip()
+
+circle = pygame.image.load('images/circle.png')
+circle = pygame.transform.scale(circle, (int(circle.get_width() * 1.1), int(circle.get_height() * 1.1)))
+pygame.display.get_surface().blit(circle, (400, 300))
 pygame.display.flip()
 
 while True:
@@ -18,22 +22,33 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                saveData.saveData({
+                    'score': saveData.loadData()['score'] + 1
+                })
+                pygame.display.get_surface().fill((0, 0, 0)) 
+                text = pygame.font.Font(None, 54).render('Score: ' + str(saveData.loadData()['score']), True, (255, 255, 255))
+                text_rect = text.get_rect(center=(35, 35))
+                pygame.display.get_surface().blit(text, (35, 35))
+                pygame.display.flip()
+
+                circle = pygame.image.load('images/circle.png')
+                circle = pygame.transform.scale(circle, (int(circle.get_width() * 1.1), int(circle.get_height() * 1.1)))
+                pygame.display.get_surface().blit(circle, (400, 300))
+                pygame.display.flip()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 saveData.resetData()
                 pygame.display.get_surface().fill((0, 0, 0))
                 text = pygame.font.Font(None, 54).render('Score: ' + str(saveData.loadData()['score']), True, (255, 255, 255))
                 text_rect = text.get_rect(center=(35, 35))
-                pygame.display.get_surface().blit(text, (0, 0))
+                pygame.display.get_surface().blit(text, (35, 35))
                 pygame.display.flip()
-            if event.key == pygame.K_SPACE:
-                saveData.saveData({
-                    'score': saveData.loadData()['score'] + 1
-                })
-                pygame.display.get_surface().fill((0, 0, 0))  # Clear the screen before drawing new text
-                text = pygame.font.Font(None, 54).render('Score: ' + str(saveData.loadData()['score']), True, (255, 255, 255))
-                text_rect = text.get_rect(center=(35, 35))
-                pygame.display.get_surface().blit(text, (0, 0))
+
+                circle = pygame.image.load('images/circle.png')
+                circle = pygame.transform.scale(circle, (int(circle.get_width() * 1.1), int(circle.get_height() * 1.1)))
+                pygame.display.get_surface().blit(circle, (400, 300))
                 pygame.display.flip()
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
